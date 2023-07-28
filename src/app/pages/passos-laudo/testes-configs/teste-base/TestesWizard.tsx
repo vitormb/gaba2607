993f958e2f261b3b8ed2ae52a4fe6faaa64d3f8b
vitormb <vitormtv2@gmail.com>
2023-07-28 01:08:15 -0300

    x

--- src/app/pages/passos-laudo/testes-configs/teste-base/TestesWizard.tsx
+++ src/app/pages/passos-laudo/testes-configs/teste-base/TestesWizard.tsx
@@ -2,28 +2,12 @@ import React, {FC, useState} from 'react'
 import {TestesSelecionados} from '../teste-base/TestesSelecionados'
 import {SubTestesContent} from '../teste-base/SubTestesContent'
 
-export const TestesWizard: React.FC = (PropFinal) => {
-  const [selectedItems, setSelectedItems] = useState<any[]>([])
-  const valorIC = valor  
-  const isChecked = selectedItems.includes(valor)  
-  const handleConfigureTest = (item: any) => {
-    //console.log(item);
-  }  
-  const ProPFinal = {
-    nomeDoTeste: valorIC,
-  }
-  const handleCheckboxChange = (valor: any) => {
-    if (selectedItems.includes(valor)) {
-      setSelectedItems(selectedItems.filter((item) => item !== valor))
-      console.log('Estes foram os selecionados: ', selectedItems)
-    } else {
-      setSelectedItems([...selectedItems, valor])
-    }
-  }
+export const TestesWizard: React.FC = (PropFinal) => {    
+    const valor = PropFinal;
   return (
-    <div>
-      <TestesSelecionados />
+    <div>      
       <SubTestesContent valor='aa' idTeste='bbb' />
+      <TestesSelecionados/>
     </div>
   )
 }
