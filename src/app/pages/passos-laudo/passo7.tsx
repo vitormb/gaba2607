4f19a43e25d66c99bfe2e3e887cae9ca747fbe96
vitormb <vitormtv2@gmail.com>
2023-07-30 00:23:56 -0300

    asdx

--- src/app/pages/passos-laudo/passo7.tsx
+++ src/app/pages/passos-laudo/passo7.tsx
@@ -1,11 +1,13 @@
 import React, {FC, useState} from 'react'
 import {ProgressBarra} from '../CadastroPacienteWizard'
 import {CreateAccordionElement} from './testes-configs/teste-base/CreateAccordionElement'
-
+import {ModalTestesSelecionados} from './testes-configs/teste-base/ModalTestesSelecionados'
 import {TestesSelecionados} from './testes-configs/teste-base/TestesSelecionados'
-import {SubTestesContent} from './testes-configs/teste-base/SubTestesContent'
+import {SubTestesContent} from './testes-configs/teste-base/SubTestesContent' 
+
 
 const Passo7: FC = () => {
+  const [selectedItems, setSelectedItems] = useState<any[]>([]);
   return (
     <div className='container-flex'>
       <div className='mt-5 col-12 mb-5'>
@@ -43,23 +45,27 @@ const Passo7: FC = () => {
                 <SubTestesContent
                   idTeste='Bateria Fatorial de Personalidade'
                   valor='Bateria Fatorial de Personalidade'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
                 <SubTestesContent
                   idTeste='CAT- A - Teste de Apercepção Infantil'
                   valor='CAT- A - Teste de Apercepção Infantil'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
-                <SubTestesContent idTeste='Escalas Beck' valor='Escalas Beck' />
-                <SubTestesContent idTeste='HTP' valor='HTP' />
+                <SubTestesContent idTeste='Escalas Beck' valor='Escalas Beck'  selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
+                <SubTestesContent idTeste='HTP' valor='HTP' selectedItems={selectedItems} setSelectedItems={setSelectedItems} />
                 <SubTestesContent
                   idTeste='Inventário de habilidades sociais'
                   valor='Inventário de habilidades sociais'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
-                <SubTestesContent idTeste='Palográfico' valor='Palográfico' />
+                <SubTestesContent idTeste='Palográfico' valor='Palográfico' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
                 <SubTestesContent
                   idTeste='Pirâmides coloridas de Pfister'
                   valor='Pirâmides coloridas de Pfister'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
-                <SubTestesContent idTeste='Zulliger' valor='Zulliger' />
+                <SubTestesContent idTeste='Zulliger' valor='Zulliger' selectedItems={selectedItems} setSelectedItems={setSelectedItems} />
               </div>
             }
           />
@@ -70,20 +76,22 @@ const Passo7: FC = () => {
             cogarea='Atenção'
             content={
               <div className='card shadow-sm parent-hover'>
-                <SubTestesContent idTeste='Coleção AD e AS' valor='Coleção AD e AS' />
-                <SubTestesContent idTeste='FDT' valor='FDT' />
-                <SubTestesContent idTeste='Neupsilin' valor='Neupsilin' />
+                <SubTestesContent idTeste='Coleção AD e AS' valor='Coleção AD e AS' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
+                <SubTestesContent idTeste='FDT' valor='FDT' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
+                <SubTestesContent idTeste='Neupsilin' valor='Neupsilin' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
                 <SubTestesContent
                   idTeste='Teste AC de Atenção Concentrada'
                   valor='Teste AC de Atenção Concentrada'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
-                <SubTestesContent idTeste='D2R' valor='D2R' />
-                <SubTestesContent idTeste='BPA' valor='BPA' />
+                <SubTestesContent idTeste='D2R' valor='D2R' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
+                <SubTestesContent idTeste='BPA' valor='BPA' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
                 <SubTestesContent
                   idTeste='Teste de cancelamento de sinos'
                   valor='Teste de cancelamento de sinos'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
-                <SubTestesContent idTeste='TAVIS' valor='TAVIS' />
+                <SubTestesContent idTeste='TAVIS' valor='TAVIS' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
               </div>
             }
           />
@@ -94,20 +102,23 @@ const Passo7: FC = () => {
             cogarea='Linguagem'
             content={
               <div className='card shadow-sm parent-hover'>
-                <SubTestesContent idTeste='Confias' valor='Confias' />
+                <SubTestesContent idTeste='Confias' valor='Confias' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
                 <SubTestesContent
                   idTeste='Manual da Escala de Tdah - versão para professores'
                   valor='Manual da Escala de Tdah - versão para professores'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
                 <SubTestesContent
                   idTeste='Teste de Compreensão Leitora de Textos Descritivos'
                   valor='Teste de Compreensão Leitora de Textos Descritivos'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
                 <SubTestesContent
                   idTeste='Teste de Habilidades e Conhecimento Pré-Alfabetização'
                   valor='Teste de Habilidades e Conhecimento Pré-Alfabetização'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
-                <SubTestesContent idTeste='TENA' valor='TENA' />
+                <SubTestesContent idTeste='TENA' valor='TENA' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
               </div>
             }
           />
@@ -118,13 +129,14 @@ const Passo7: FC = () => {
             cogarea='Habilidades acadêmicas'
             content={
               <div className='card shadow-sm parent-hover'>
-                <SubTestesContent idTeste='CORUJA PRO-MAT' valor='CORUJA PRO-MAT' />
+                <SubTestesContent idTeste='CORUJA PRO-MAT' valor='CORUJA PRO-MAT' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
                 <SubTestesContent
                   idTeste='Teste de Desempenho Escolar'
                   valor='Teste de Desempenho Escolar'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
-                <SubTestesContent idTeste='CORUJA ESPECIALISTA' valor='CORUJA ESPECIALISTA' />
-                <SubTestesContent idTeste='TDE 2' valor='TDE 2' />
+                <SubTestesContent idTeste='CORUJA ESPECIALISTA' valor='CORUJA ESPECIALISTA' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
+                <SubTestesContent idTeste='TDE 2' valor='TDE 2' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
               </div>
             }
           />
@@ -138,35 +150,42 @@ const Passo7: FC = () => {
                 <SubTestesContent
                   idTeste='Desenho da Figura Humana'
                   valor='Desenho da Figura Humana'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
                 <SubTestesContent
                   idTeste='Escala de Maturidade Mental - COLUMBIA'
                   valor='Escala de Maturidade Mental - COLUMBIA'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
                 <SubTestesContent
                   idTeste='Escala de Traços de Personalidade para Crianças'
                   valor='Escala de Traços de Personalidade para Crianças'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
                 <SubTestesContent
                   idTeste='Figuras Complexas de Rey'
                   valor='Figuras Complexas de Rey'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
                 <SubTestesContent
                   idTeste='R-2 - Raven para crianças'
                   valor='R-2 - Raven para crianças'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
-                <SubTestesContent idTeste='SON R' valor='SON R' />
+                <SubTestesContent idTeste='SON R' valor='SON R' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
                 <SubTestesContent
                   idTeste='Teste de Memória de Reconhecimento - TEM-R'
                   valor='Teste de Memória de Reconhecimento - TEM-R'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
                 <SubTestesContent
                   idTeste='Teste não verbal de Inteligência Geral - BETA III'
                   valor='Teste não verbal de Inteligência Geral - BETA III'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
-                <SubTestesContent idTeste='WAIS' valor='WAIS' />
-                <SubTestesContent idTeste='WASI' valor='WASI' />
-                <SubTestesContent idTeste='WISC 4' valor='WISC 4' />
+                <SubTestesContent idTeste='WAIS' valor='WAIS' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
+                <SubTestesContent idTeste='WASI' valor='WASI' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
+                <SubTestesContent idTeste='WISC 4' valor='WISC 4' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
               </div>
             }
           />
@@ -180,8 +199,9 @@ const Passo7: FC = () => {
                 <SubTestesContent
                   idTeste='Manual do Inventário de Sintomas de Stress para Adultos de Lipp - ISSL'
                   valor='Manual do Inventário de Sintomas de Stress para Adultos de Lipp - ISSL'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
-                <SubTestesContent idTeste='PERFIL SENSORIAL' valor='PERFIL SENSORIAL' />
+                <SubTestesContent idTeste='PERFIL SENSORIAL' valor='PERFIL SENSORIAL' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
               </div>
             }
           />
@@ -195,13 +215,15 @@ const Passo7: FC = () => {
                 <SubTestesContent
                   idTeste='Figuras Complexas de Rey'
                   valor='Figuras Complexas de Rey'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
-                <SubTestesContent idTeste='Neupsilin' valor='Neupsilin' />
+                <SubTestesContent idTeste='Neupsilin' valor='Neupsilin' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
                 <SubTestesContent
                   idTeste='Teste Gestaltico Visomotor de Bender'
                   valor='Teste Gestaltico Visomotor de Bender'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
-                <SubTestesContent idTeste='RAVLT' valor='RAVLT' />
+                <SubTestesContent idTeste='RAVLT' valor='RAVLT' selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
               </div>
             }
           />
@@ -215,14 +237,17 @@ const Passo7: FC = () => {
                 <SubTestesContent
                   idTeste='Inventário de habilidades sociais'
                   valor='Inventário de habilidades sociais'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
                 <SubTestesContent
                   idTeste='Sistema Multimídia de habilidades sociais de crianças'
                   valor='Sistema Multimídia de habilidades sociais de crianças'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
                 <SubTestesContent
                   idTeste='SSRS - Inventário de Habilidades Sociais, Problemas de Comportamento e Competência Acadêmica para Crianças'
                   valor='SSRS - Inventário de Habilidades Sociais, Problemas de Comportamento e Competência Acadêmica para Crianças'
+                  selectedItems={selectedItems} setSelectedItems={setSelectedItems}
                 />
               </div>
             }
@@ -230,7 +255,8 @@ const Passo7: FC = () => {
         </div>
       </div>
       <div className='separator my-10'></div>
-      <TestesSelecionados selectedItems={[]} valor='asdx' handleConfigureTest={SubTestesContent} />
+      <ModalTestesSelecionados selectedItems={selectedItems} valor='asdx' handleConfigureTest={SubTestesContent} />
+      <TestesSelecionados selectedItems={selectedItems} valor='asdx' handleConfigureTest={SubTestesContent} />
     </div>
   )
 }
