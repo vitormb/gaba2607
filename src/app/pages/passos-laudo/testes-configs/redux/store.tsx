db39f5fd803ab3b36719af98a184365fd64bc8fb
vitormb <vitormtv2@gmail.com>
2023-08-01 17:53:35 -0300

    wasx

--- src/app/pages/passos-laudo/testes-configs/redux/store.tsx
+++ src/app/pages/passos-laudo/testes-configs/redux/store.tsx
@@ -1,7 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import categoriasReducer from './categorias/categoriasSlice';
 import testesReducer from './testes/testesSlice';
 import subtestesReducer from './subtestes/subtestesSlice';
+
+// Seu middleware personalizado
+const customMiddleware = (storeAPI:any) => (next:any) => (action:any) => {
+  console.log('Despachando ação:', action);
+
+  return next(action);
+};
 
 export default configureStore({
   reducer: {
@@ -9,4 +16,5 @@ export default configureStore({
     testes: testesReducer,
     subtestes: subtestesReducer,
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(customMiddleware),
 });
--- src/app/pages/passos-laudo/testes-configs/redux/testes/Teste.tsx
+++ src/app/pages/passos-laudo/testes-configs/redux/testes/Teste.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import SubtestesList from '../subtestes/SubtestesList';
+import { Teste as TesteType } from './testesSlice';
 
-const Teste: React.FC<{ teste: any }> = ({ teste }) => {
-  return <div>{teste.nome}</div>;
+const Teste: React.FC<{ teste: TesteType }> = ({ teste }) => {
+  return (
+    <div>
+      <h2>{teste.nome}</h2>
+      <SubtestesList subtestesIds={teste.subtestes} />
+    </div>
+  );
 };
 
 export default Teste;
